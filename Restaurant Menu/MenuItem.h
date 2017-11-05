c5a556db3f5ae423376cf28b099e6f997c5c3f3b
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2017-11-05 12:21:02 +0200

    ADD: Menu Item model

---
+++ Restaurant Menu/MenuItem.h
@@ -0,0 +1,13 @@
+//
+//  MenuItem.h
+//  Restaurant Menu
+//
+//  Created by Shaker on 11/5/17.
+//  Copyright © 2017 Shaker. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface MenuItem : JSONModel
+
+@end
---
+++ Restaurant Menu/MenuItem.m
@@ -0,0 +1,13 @@
+//
+//  MenuItem.m
+//  Restaurant Menu
+//
+//  Created by Shaker on 11/5/17.
+//  Copyright © 2017 Shaker. All rights reserved.
+//
+
+#import "MenuItem.h"
+
+@implementation MenuItem
+
+@end
