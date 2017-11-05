abfad78cfa02c5c5575ee08e9c3e74e82584b2c5
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2017-11-05 12:20:47 +0200

    ADD: Menu Category model

---
+++ Restaurant Menu/MenuCategory.h
@@ -0,0 +1,13 @@
+//
+//  MenuCategory.h
+//  Restaurant Menu
+//
+//  Created by Shaker on 11/5/17.
+//  Copyright © 2017 Shaker. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface MenuCategory : JSONModel
+
+@end
---
+++ Restaurant Menu/MenuCategory.m
@@ -0,0 +1,13 @@
+//
+//  MenuCategory.m
+//  Restaurant Menu
+//
+//  Created by Shaker on 11/5/17.
+//  Copyright © 2017 Shaker. All rights reserved.
+//
+
+#import "MenuCategory.h"
+
+@implementation MenuCategory
+
+@end
