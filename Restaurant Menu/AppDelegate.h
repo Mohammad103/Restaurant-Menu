952c2131378febc02a8e61fcd73b85c64752f635
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2017-11-05 12:00:35 +0200

    Initial Commit

---
+++ Restaurant Menu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Restaurant Menu
+//
+//  Created by Shaker on 11/5/17.
+//  Copyright © 2017 Shaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Restaurant Menu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Restaurant Menu
+//
+//  Created by Shaker on 11/5/17.
+//  Copyright © 2017 Shaker. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
